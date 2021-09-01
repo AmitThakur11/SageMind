57209433f5e26196378f731149a6c1b572ccba40
amit thakur <amithakur081@gmail.com>
2021-09-01 15:18:25 +0530

    Add : context

---
+++ src/Context/AuthContext.tsx
@@ -0,0 +1,41 @@
+import { useReducer } from "react"
+import {useState, useContext , createContext, ReactNode } from "react"
+import { InitialAuthState , AuthDispatch , AuthAction } from "../Types/AuthType"
+
+
+export const AuthContext = createContext<{authState : InitialAuthState , authDispatch : AuthDispatch}>({} as {authState : InitialAuthState , authDispatch : AuthDispatch})
+
+export const initialAuthState : InitialAuthState = {
+    login : false,
+    user : {
+        username : "",
+        email : "",
+        password : "",
+        cpassword : ""
+    }
+}
+
+export const authReducer = (authState:InitialAuthState , action : AuthAction)=>{
+    switch(action.type){
+        case "LOGIN":{
+            return {...authState , login :true }
+        }
+    }
+
+}
+
+const AuthProvider = ({children}:{children : ReactNode})=>{
+
+    const [authState , authDispatch] = useReducer(authReducer , initialAuthState)
+    
+    return(
+        <AuthContext.Provider value ={{authState , authDispatch}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = ()=>useContext(AuthContext)
+
+
+export default AuthProvider
---
+++ src/Context/QuizContext.tsx
@@ -0,0 +1,76 @@
+import { useContext, createContext, useReducer, ReactNode } from "react";
+import quizData from "../database";
+
+import { InitialState, Dispatch, Action, QuizData } from "../Types/QuizType";
+export const quizContext = createContext<{
+  state: InitialState;
+  dispatch: Dispatch;
+}>({} as { state: InitialState; dispatch: Dispatch });
+
+export const initialState: InitialState = {
+  quiz_id: 0,
+  questionsAnswered: [],
+  score: 0,
+  current: 0,
+};
+
+export const reducer = (state: InitialState, action: Action): InitialState => {
+  const { id, answer, quizAnswer } = action.payload;
+  switch (action.type) {
+    case "SELECT QUIZ": {
+      const findQuiz = quizData.find((quiz) => quiz.id === id) as QuizData;
+      return { ...state, quiz_id: findQuiz.id as number };
+    }
+    case "SELECT ANSWER": {
+      if (answer) {
+        return {
+          ...state,
+          current: state.current + 1,
+          score: state.score + 10,
+          questionsAnswered: [
+            ...state.questionsAnswered,
+            {
+              question: quizAnswer?.question,
+              chosenValue: quizAnswer?.chosenValue,
+              yourChoice: answer,
+            },
+          ],
+        };
+      }
+      return {
+        ...state,
+        current: state.current + 1,
+        questionsAnswered: [
+          ...state.questionsAnswered,
+          {
+            question: quizAnswer?.question,
+            chosenValue: quizAnswer?.chosenValue,
+            yourChoice: false,
+          },
+        ],
+      };
+    }
+
+    case "RESET":
+      return (state = initialState);
+
+    default:
+      return state;
+  }
+};
+
+const QuizProvider = ({ children }: { children: ReactNode }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <quizContext.Provider value={{ state, dispatch }}>
+      {children}
+    </quizContext.Provider>
+  );
+};
+
+export const useQuiz = () => {
+  return useContext(quizContext);
+};
+
+export default QuizProvider;
