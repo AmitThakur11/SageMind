a42669af0e327f22f4353df2a883bb29849c92ba
amit thakur <amithakur081@gmail.com>
2021-09-01 15:18:46 +0530

    Add : types

---
+++ src/Types/QuizType.ts
@@ -0,0 +1,62 @@
+export type Option = {
+    option: string;
+    isRight: boolean;
+  };
+  
+  export type Quiz = {
+    id: number;
+    question: string;
+    points: number;
+    options: Option[];
+  };
+  
+  export type QuizData = {
+    id: number;
+    quizName: string;
+    quizCover : string;
+    quiz: Quiz[];
+  };
+  
+  export type QuestionsAnswered = {
+    question?: string;
+    chosenValue?: string;
+    yourChoice?: boolean;
+    
+  };
+  
+  export type QuizAnswered = {
+    questionsAnswered: QuestionsAnswered[];
+  };
+  
+  export type InitialState = {
+    quiz_id: number;
+    questionsAnswered: QuestionsAnswered[];
+    score: number;
+    current: number;
+  };
+  
+  export type QuizAnswer = {
+    question?: string;
+    chosenValue?: string;
+
+  }
+  export type  Payload = {
+    id?: number;
+    answer?: boolean;
+    quizAnswer? : QuizAnswer 
+
+  }
+  export type Action = {
+    type: "SELECT QUIZ" | "SELECT ANSWER" | "RESET";
+    payload: Payload 
+  };
+  
+  export type Dispatch = (action: Action) => void;
+  
+  export type PassValue = {
+    state: InitialState;
+    dispatch: Dispatch;
+  };
+
+  export type Rules = []
+  
