1a0befe0567560f79ff10c1a5744d83924b2af7e
amit thakur <amithakur081@gmail.com>
2021-11-07 20:03:31 +0530

    Add : icons

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,36 +1,52 @@
 import "./Header.css";
-import {useState} from "react"
-import {Link} from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { GrGithub, GrTwitter } from "react-icons/gr";
 const Header = () => {
-  const [menu , setMenu] = useState(false)
+  const [menu, setMenu] = useState(false);
   return (
-    <section className ="navbar">
+    <section className="navbar">
       <header>
         <div className="menu">
-          <i onClick={()=>setMenu(!menu)} className={menu ? `fa fa-close` : `fa fa-reorder`}></i>
+          <i
+            onClick={() => setMenu(!menu)}
+            className={menu ? `fa fa-close` : `fa fa-reorder`}
+          ></i>
         </div>
         <div className="logo">
-         <img src="https://i.ibb.co/cFjKjRQ/003-ninja.png"alt ="logo"/>
+          <img src="https://i.ibb.co/cFjKjRQ/003-ninja.png" alt="logo" />
           <Link to="/">
-          <div className ="name">
-            <div className="first">SAGE</div>
-            <div className="last">MIND</div>
+            <div className="name">
+              <div className="first">SAGE</div>
+              <div className="last">MIND</div>
+            </div>
+          </Link>
         </div>
-        </Link>
-        </div>
-        <div className= {menu ? `header-options active ` : `header-options off ` } >
+        <div
+          className={menu ? `header-options active ` : `header-options off `}
+        >
           <div className="sidebar-logo">
-          <img src="https://i.ibb.co/cFjKjRQ/003-ninja.png"alt ="logo"/>
-          <div className ="name">
-            <div className="first">SAGE</div>
-            <div className="last">MIND</div>
-        </div>
+            <img src="https://i.ibb.co/cFjKjRQ/003-ninja.png" alt="logo" />
+            <div className="name">
+              <div className="first">SAGE</div>
+              <div className="last">MIND</div>
+            </div>
           </div>
-          <Link to ="/"><div>QUIZ</div></Link>
-          {/* <Link to ="/leaderboard"><div>LEADERBOARD</div></Link> */}
-          <Link to ="/about"><div>ABOUT</div></Link>
-          {/* <Link to="/login"><div  className="login-btn">{authState.login ? `Log out` : `Log in`}</div></Link> */}
+          <Link to="/">
+            <div>QUIZ</div>
+          </Link>
           
+            <a href ="https://github.com/AmitThakur11">
+              <GrGithub />
+              </a>
+            
+         
+          
+            <a href="https://twitter.com/_amit__thakur">
+              <GrTwitter  />
+            </a>
+          
+          {/* <Link to="/login"><div  className="login-btn">{authState.login ? `Log out` : `Log in`}</div></Link> */}
         </div>
       </header>
     </section>
